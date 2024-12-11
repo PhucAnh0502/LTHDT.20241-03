faef855ed5082e14ee631578f34813a86a0352b2
Nguyen Hai Anh <160818903+haianh1204@users.noreply.github.com>
2024-12-11 09:42:50 +0700

    Update InputController.java

--- sourcecode/application/controllers/InputController.java
+++ sourcecode/application/controllers/InputController.java
@@ -1,7 +1,29 @@
 package application.controllers;
 
 public class InputController {
+	   private void generateRandomArray() {
+        Random rand = new Random();
+        int n = rand.nextInt(10) + 5; // Array size between 5 and 14
+        array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = rand.nextInt(100); // Random numbers between 0 and 99
+        }
+        arrayInput.setText(Arrays.toString(array));
+        outputArea.setText("Generated Random Array: \n" + Arrays.toString(array));
+    }
 
-	private DataSortingController dataSortingController;
-
+    // Method to input array manually
+    private void inputArray() {
+        try {
+            String input = arrayInput.getText();
+            String[] inputValues = input.replaceAll("[\\[\\] ]", "").split(",");
+            array = new int[inputValues.length];
+            for (int i = 0; i < inputValues.length; i++) {
+                array[i] = Integer.parseInt(inputValues[i].trim());
+            }
+            outputArea.setText("Input Array: \n" + Arrays.toString(array));
+        } catch (NumberFormatException e) {
+            outputArea.setText("Invalid input format. Please use a comma-separated list.");
+        }
+    }
 }
