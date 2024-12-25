01e290a0f0dd54567b8a0e45acdcd8ac94825555
NguyenKyAnh-20225783 <nguyenkyanh22122003@gmail.com>
2024-12-25 21:55:30 +0700

    update

--- sourcecode/application/algorithm/ShellSort.java
+++ sourcecode/application/algorithm/ShellSort.java
@@ -2,25 +2,37 @@ package application.algorithm;
 
 import application.panel.SortingPanel;
 
-public class ShellSort extends SortingAlgorithm {
-	// Additional attribute: gap
+public class ShellSort extends SortingAlgorithm{
+	// add attrinute: gap
 	private int gap;
-    @Override
-    public void sort(SortingPanel panel) {
-        array = panel.getArray();  // Get the array from the panel
-        if (array == null || array.length <= 1) return;
 
-        for (gap = array.length / 2; gap > 0; gap /= 2) {
-            for (int i = gap; i < array.length; i++) {
-                int j = i;
-                while (j >= gap && array[j - gap] > array[j]) {
-                    panel.setCompareIndices(j, j - gap);
-                    panel.setSwapIndices(j, j - gap);
-                    j -= gap;
-                }
-                panel.setArray(array);  // Update the array in the panel
-            }
-        }
-        panel.setArray(array);  // Finalize the array in the panel after sorting
-    }
+	@Override
+	public void sort(SortingPanel panel) {
+		// TODO Auto-generated method stub
+		array = panel.getArray();//lay mang tu panel
+		if(array == null|| array.length <=1)
+		{
+			return ;
+		}
+		for(gap = array.length/2;gap>0;gap/=2)// chia mang
+	
+		{
+			for(int i=gap ; i<array.length; i++) //duyet phan tu ben phai
+			{
+				int j=i;
+				while(j>=gap&&array[j-gap]>array[j])// so sanh 2 ptu ben trai va ben phai
+				{   // cap nhat chi so va so sanh
+					panel.setCompareIndices(j, j-gap);
+					panel.setSwapIndices(j, j-gap);
+					j-=gap;
+				}
+				// cap nhat sau moi lan chay xong vong lap
+
+				panel.setArray(array);
+			}
+		}
+		panel.setArray(array);
+		
+	}
+	
 }
